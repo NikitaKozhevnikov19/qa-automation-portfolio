65aae63cde2de80708bddeeab1eb239e0bbd43a2
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-10-21 22:06:10 +0500

    update CalendarComponent.java

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,17 +1,27 @@
 package pages.components;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
     private SelenideElement yearPicker = $(".react-datepicker__year-select"),
             monthPicker = $(".react-datepicker__month-select");
-    private String dayPicker = ".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)";
 
     public void setDate(String day, String month, String year) {
         yearPicker.selectOption(year);
         monthPicker.selectOption(month);
-        $(String.format(dayPicker, day)).click();
+
+
+        $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)")
+                .shouldBe(sizeGreaterThan(0));
+
+        $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)")
+                .findBy(Condition.text(String.valueOf(Integer.parseInt(day))))
+                .shouldBe(Condition.visible)
+                .click();
     }
 }
