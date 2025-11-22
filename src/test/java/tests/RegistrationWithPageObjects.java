1e2f7ee971c8fed6d14326e732f3abee60e850ad
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-11-22 22:01:01 +0500

    changed RegistrationWithPageObjects.java

--- src/test/java/tests/RegistrationWithPageObjects.java
+++ src/test/java/tests/RegistrationWithPageObjects.java
@@ -1,11 +1,14 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import pages.components.ModalComponent;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
 
 public class RegistrationWithPageObjects extends TestBase {
 
@@ -13,56 +16,44 @@ public class RegistrationWithPageObjects extends TestBase {
     ModalComponent modalComponent = new ModalComponent();
 
     @Test
+    @Tag("demoqa")
     void formTest() {
 
-        registrationPage.openPage()
-                .setFirstName("Nikita")
-                .setLastName("Kozhevnikov")
-                .setEmail("kozherka2@gamil.com")
-                .setGender("Male")
-                .setPhoneNumber("9999999999")
-                .setDateOfBirth("19", "January", "2001")
-                .setSubject("Maths")
-                .setHobby("Sports")
-                .setPicture("12.jpg")
-                .setAddress("Some address")
-                .setState("NCR")
-                .setCity("Delhi")
-                .submitForm();
-        modalComponent.checkModalIsOpen()
-                .checkResult("Student Name", "Nikita Kozhevnikov")
-                .checkResult("Student Email", "kozherka2@gamil.com")
-                .checkResult("Gender", "Male")
-                .checkResult("Mobile", "9999999999")
-                .checkResult("Date of Birth", "19 January,2001")
-                .checkResult("Subjects", "Maths")
-                .checkResult("Hobbies", "Sports")
-                .checkResult("Picture", "12.jpg")
-                .checkResult("Address", "Some address")
-                .checkResult("State and City", "NCR Delhi")
-                .closeModal();
-    }
+        step("Open form", () -> {
+            registrationPage.openPage();
+        });
 
-    @Test
-    void fillMinimalFormTest() {
-        registrationPage.openPage()
-                .setFirstName("Nikita")
-                .setLastName("Kozhevnikov")
-                .setGender("Male")
-                .setPhoneNumber("9999999999")
-                .submitForm();
-        modalComponent.checkModalIsOpen()
-                .checkResult("Student Name", "Nikita Kozhevnikov")
-                .checkResult("Gender", "Male")
-                .checkResult("Mobile", "9999999999")
-                .closeModal();
-    }
+        step("Fill form", () -> {
+            registrationPage
+                    .setFirstName("Nikita")
+                    .setLastName("Kozhevnikov")
+                    .setEmail("kozherka2@gamil.com")
+                    .setGender("Male")
+                    .setPhoneNumber("9999999999")
+                    .setDateOfBirth("19", "January", "2001")
+                    .setSubject("Maths")
+                    .setHobby("Sports")
+                    .setPicture("12.jpg")
+                    .setAddress("Some address")
+                    .setState("NCR")
+                    .setCity("Delhi")
+                    .submitForm();
+        });
 
-    @Test
-    void fillNegativeFormTest() {
-        registrationPage.openPage()
-                .submitForm()
-                .checkValidation();
-        modalComponent.checkModalIsNotOpen();
+        step("Verify results", () -> {
+            modalComponent.checkModalIsOpen()
+                    .checkResult("Student Name", "Nikita Kozhevnikov")
+                    .checkResult("Student Email", "kozherka2@gamil.com")
+                    .checkResult("Gender", "Male")
+                    .checkResult("Mobile", "9999999999")
+                    .checkResult("Date of Birth", "19 January,2001")
+                    .checkResult("Subjects", "Maths")
+                    .checkResult("Hobbies", "Sports")
+                    .checkResult("Picture", "12.jpg")
+                    .checkResult("Address", "Some address")
+                    .checkResult("State and City", "NCR Delhi")
+                    .closeModal();
+        });
+
     }
 }
