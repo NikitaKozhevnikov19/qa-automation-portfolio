bd6e71f18aae94360c5824514d62e965e059813e
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-12-06 23:10:50 +0500

    changed CompanyPage.java

--- src/test/java/pages/CompanyPage.java
+++ src/test/java/pages/CompanyPage.java
@@ -1,14 +1,30 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.actions;
 
 public class CompanyPage {
 
+    private final SelenideElement companyLink = $("a[href='/company/']");
+    private final SelenideElement header = $("h1");
+    private final SelenideElement aboutBlock = $(".about");
+
+
+    public CompanyPage openAboutPage() {
+        actions().moveToElement(companyLink).perform();
+        companyLink.shouldBe(visible).click();
+        return this;
+    }
+
+
     public CompanyPage checkAboutPage() {
-        $("h1").shouldHave(text("О компании"));
-        $(".company-mission").shouldBe(visible);
+        header.shouldHave(text("О компании"));
+        aboutBlock.shouldBe(visible);
         return this;
     }
 }
+
