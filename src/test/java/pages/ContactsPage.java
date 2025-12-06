33f1d3c57023040c3c5bedecc31c53a8c348c79c
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-12-06 23:14:14 +0500

    changed ContactsPage.java

--- src/test/java/pages/ContactsPage.java
+++ src/test/java/pages/ContactsPage.java
@@ -4,7 +4,7 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ContactsPage {
@@ -51,7 +51,7 @@ public class ContactsPage {
     }
 
     public ContactsPage selectTopic(String topic) {
-        topicDropdown.$$("a").findBy(byText(topic)).click();
+        topicDropdown.$$("a").findBy(text(topic)).click();
         return this;
     }
 
