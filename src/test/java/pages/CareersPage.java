964c2ae0b2da120e652bbe27899311d7b7323fdc
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-12-06 22:15:24 +0500

    update CareersPage.java

--- src/test/java/pages/CareersPage.java
+++ src/test/java/pages/CareersPage.java
@@ -9,13 +9,18 @@ import static com.codeborne.selenide.Selenide.$$;
 public class CareersPage {
 
     public CareersPage openVacancies() {
+        // Нажимаем на ссылку "Вакансии"
         $$("a.link").findBy(text("Вакансии")).click();
         return this;
     }
 
     public CareersPage checkVacanciesLoaded() {
+        // Ждём контейнер блока вакансий
         $(".vcnc-container-fluid").shouldBe(visible);
-        $$(".vcnc-vacancy-item").shouldHave(sizeGreaterThan(0));
+        // Ждём видимости хотя бы первой вакансии
+        $$(".vcnc-item").first().shouldBe(visible);
+        // Проверяем, что есть хотя бы одна вакансия
+        $$(".vcnc-item").shouldHave(sizeGreaterThan(0));
         return this;
     }
 }
