201f41e4031dd415068984c8f986df404c875871
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-12-06 20:52:06 +0500

    changed HeaderMenuComponent.java

--- src/test/java/pages/components/HeaderMenuComponent.java
+++ src/test/java/pages/components/HeaderMenuComponent.java
@@ -1,16 +1,37 @@
 package pages.components;
 
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class HeaderMenuComponent {
 
+    private final String menuItem = "header a[href='%s']";
+    private final String subMenuItem = "header a[href='%s']";
+
     public void openMenuItem(String name) {
-        $("[data-test='main-menu']").$(byText(name)).click();
+
+        // Привязываем текст к URL (это намного надежнее, чем byText)
+        String href = switch (name) {
+            case "Продукты" -> "/products/";
+            case "Компания" -> "/company/";
+            case "Карьера" -> "/career/";
+            case "Контакты" -> "/contacts/";
+            default -> throw new IllegalArgumentException("Неизвестный пункт меню: " + name);
+        };
+
+        $(String.format(menuItem, href)).shouldBe(visible).click();
     }
 
-    public void openSubMenu(String menu, String sub) {
-        $("[data-test='main-menu']").$(byText(menu)).hover();
-        $(byText(sub)).click();
+    public void openSubMenu(String main, String sub) {
+
+        openMenuItem(main);
+        String href = switch (sub) {
+            case "О нас" -> "/company/about/";
+            case "Наша история" -> "/company/history/";
+            case "Команда" -> "/company/team/";
+            default -> throw new IllegalArgumentException("Неизвестный подпункт: " + sub);
+        };
+
+        $(String.format(subMenuItem, href)).shouldBe(visible).click();
     }
 }
