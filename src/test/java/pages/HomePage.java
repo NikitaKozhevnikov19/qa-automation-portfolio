6ea763e27b614b731dc48f1f17780d9a147f2161
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-12-08 23:32:04 +0500

    update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,29 +1,32 @@
 package pages;
 
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class HomePage {
 
+    @Step("Открыть главную страницу")
     public HomePage openPage() {
         open("/");
         closeCookiesIfPresent();
-        $("a.logo").shouldBe(visible);
+        $("a.logo").shouldBe(Condition.visible);
         return this;
     }
 
-    public HomePage checkLogo() {
-        $("a.logo").shouldHave(attribute("href",
-                "https://www.naumen.ru/"));
-        return this;
-    }
-
+    @Step("Закрыть окно с cookies, если присутствует")
     public HomePage closeCookiesIfPresent() {
         if ($("button.cookie-policy__accept").exists()) {
-            $("button.cookie-policy__accept").shouldBe(visible).click();
+            $("button.cookie-policy__accept").click();
         }
+        return this;
+    }
+
+    @Step("Проверить, что логотип ведёт на URL: {expectedHref}")
+    public HomePage checkLogo(String expectedHref) {
+        $("a.logo").shouldHave(Condition.attribute("href", expectedHref));
         return this;
     }
 }
