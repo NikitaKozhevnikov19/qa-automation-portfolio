06e7d879468cdab96e0a9e9ab8c6e421b846d787
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-12-24 21:54:32 +0500

    added owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,8 +3,10 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,28 +18,27 @@ public class TestBase {
 
     @BeforeAll
     static void onSettings() {
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-
-        String remoteUrl = System.getProperty("remote");
-        if (remoteUrl != null && !remoteUrl.isEmpty()) {
-            Configuration.remote = remoteUrl;
-        }
-
+        String env = System.getProperty("env", "local");
+        System.setProperty("env", env);
+        WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
+        Configuration.browser = config.browserName();
+        Configuration.browserVersion = config.browserVersion();
         Configuration.baseUrl = "https://www.naumen.ru";
+        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.pageLoadTimeout = 60000;
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", Configuration.browser);
-        capabilities.setCapability("browserVersion", Configuration.browserVersion);
-        capabilities.setCapability("selenoid:options", Map.of(
-                "enableVNC", true,
-                "enableVideo", true,
-                "enableFileUploads", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+
+        if (config.isRemote()) {
+            Configuration.remote = config.remoteUrl();
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.of(
+                    "enableVNC", config.enableVNC(),
+                    "enableVideo", config.enableVideo(),
+                    "enableFileUploads", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
     }
 
     @BeforeEach
