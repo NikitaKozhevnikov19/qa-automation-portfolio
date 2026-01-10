477417d59251c08214d9374f734e176c04fcedba
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-01-10 22:30:21 +0500

    update ProductsPage.java

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,15 +1,18 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class ProductsPage {
 
+    private final SelenideElement productsBlock = $(".about__products");
+
     @Step("Проверить, что продукты загрузились")
     public ProductsPage checkProductsLoaded() {
-        $(".about__products").shouldBe(Condition.visible);
+        productsBlock.shouldBe(Condition.visible);
         return this;
     }
 }
