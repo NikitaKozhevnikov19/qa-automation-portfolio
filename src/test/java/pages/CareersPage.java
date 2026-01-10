1d7cd77aefc515f8374e0fd40b26bfac5435e880
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-01-10 22:29:39 +0500

    update CareersPage.java

--- src/test/java/pages/CareersPage.java
+++ src/test/java/pages/CareersPage.java
@@ -2,24 +2,31 @@ package pages;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-
 public class CareersPage {
+
+    private final SelenideElement
+            vacanciesLink = $$("a.link").findBy(Condition.text("Вакансии")),
+            vacanciesContainer = $(".vcnc-container-fluid");
+
+    private final ElementsCollection vacancyItems = $$(".vcnc-item");
 
     @Step("Открыть список вакансий")
     public CareersPage openVacancies() {
-        $$("a.link").findBy(Condition.text("Вакансии")).click();
+        vacanciesLink.click();
         return this;
     }
 
     @Step("Проверить, что вакансии загрузились")
     public CareersPage checkVacanciesLoaded() {
-        $(".vcnc-container-fluid").shouldBe(Condition.visible);
-        $$(".vcnc-item").shouldHave(CollectionCondition.sizeGreaterThan(0));
+        vacanciesContainer.shouldBe(Condition.visible);
+        vacancyItems.shouldHave(CollectionCondition.sizeGreaterThan(0));
         return this;
     }
 }
