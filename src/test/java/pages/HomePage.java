ed0408c2181b0998a0a812efe2cdc333f2fe85c1
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-01-10 22:29:51 +0500

    update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -8,25 +9,29 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class HomePage {
 
+    private final SelenideElement
+            logo = $("a.logo"),
+            cookieAcceptButton = $("button.cookie-policy__accept");
+
     @Step("Открыть главную страницу")
     public HomePage openPage() {
         open("/");
         closeCookiesIfPresent();
-        $("a.logo").shouldBe(Condition.visible);
+        logo.shouldBe(Condition.visible);
         return this;
     }
 
     @Step("Закрыть окно с cookies, если присутствует")
     public HomePage closeCookiesIfPresent() {
-        if ($("button.cookie-policy__accept").exists()) {
-            $("button.cookie-policy__accept").click();
+        if (cookieAcceptButton.exists()) {
+            cookieAcceptButton.click();
         }
         return this;
     }
 
     @Step("Проверить, что домашняя страница открылась")
     public HomePage checkHomePageIsOpened() {
-        $("a.logo").shouldHave(Condition.attribute("href", "https://www.naumen.ru/"));
+        logo.shouldHave(Condition.attribute("href", "https://www.naumen.ru/"));
         return this;
     }
 }
